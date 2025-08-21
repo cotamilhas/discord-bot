75c8748ce51f4c3eeabd5f50bc59d4e805d9a870
Henrique <104533111+cotamilhas@users.noreply.github.com>
2025-08-21 02:28:16 +0100

    Leaderboard Fix
    I also wanna do level cards like mee6

--- cogs/leveling.py
+++ cogs/leveling.py
@@ -11,10 +11,10 @@ from config import EMBED_COLOR, LEVELS_FILE
 class Leveling(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.levels = self.loadLevels()
+        self.levels = self.load_levels()
         self.cooldowns = {}
 
-    def loadLevels(self):
+    def load_levels(self):
         if os.path.exists(LEVELS_FILE):
             try:
                 with open(LEVELS_FILE, 'r') as f:
@@ -24,11 +24,11 @@ class Leveling(commands.Cog):
                 return {}
         return {}
 
-    def saveLevels(self):
+    def save_levels(self):
         with open(LEVELS_FILE, 'w') as f:
             json.dump(self.levels, f, indent=4)
 
-    def getRequiredXp(self, level):
+    def get_required_xp(self, level):
         return 100 + (level - 1) * 50
 
     @commands.Cog.listener()
@@ -36,36 +36,36 @@ class Leveling(commands.Cog):
         if message.author.bot or not message.guild:
             return
 
-        guildId = str(message.guild.id)
-        memberId = str(message.author.id)
-        guildName = message.guild.name
+        guild_id = str(message.guild.id)
+        member_id = str(message.author.id)
+        guild_name = message.guild.name
 
-        if guildId not in self.levels:
-            self.levels[guildId] = {"serverName": guildName, "members": {}}
+        if guild_id not in self.levels:
+            self.levels[guild_id] = {"server_name": guild_name, "members": {}}
 
-        if memberId not in self.levels[guildId]["members"]:
-            self.levels[guildId]["members"][memberId] = {"xp": 0, "level": 1}
+        if member_id not in self.levels[guild_id]["members"]:
+            self.levels[guild_id]["members"][member_id] = {"xp": 0, "level": 1}
 
-        if memberId in self.cooldowns:
-            if self.cooldowns[memberId] > asyncio.get_event_loop().time():
+        if member_id in self.cooldowns:
+            if self.cooldowns[member_id] > asyncio.get_event_loop().time():
                 return
-        self.cooldowns[memberId] = asyncio.get_event_loop().time() + 5
+        self.cooldowns[member_id] = asyncio.get_event_loop().time() + 5
 
-        xpGain = random.randint(5, 15)
-        self.levels[guildId]["members"][memberId]["xp"] += xpGain
-        xp = self.levels[guildId]["members"][memberId]["xp"]
-        level = self.levels[guildId]["members"][memberId]["level"]
+        xp_gain = random.randint(5, 15)
+        self.levels[guild_id]["members"][member_id]["xp"] += xp_gain
+        xp = self.levels[guild_id]["members"][member_id]["xp"]
+        level = self.levels[guild_id]["members"][member_id]["level"]
 
-        requiredXp = self.getRequiredXp(level)
-        if xp >= requiredXp:
-            excessXp = xp - requiredXp
-            self.levels[guildId]["members"][memberId]["level"] += 1
-            self.levels[guildId]["members"][memberId]["xp"] = excessXp
+        required_xp = self.get_required_xp(level)
+        if xp >= required_xp:
+            excess_xp = xp - required_xp
+            self.levels[guild_id]["members"][member_id]["level"] += 1
+            self.levels[guild_id]["members"][member_id]["xp"] = excess_xp
 
-            newLevel = self.levels[guildId]["members"][memberId]["level"]
-            await message.channel.send(f"{message.author.mention}, you leveled up to **{newLevel}**!")
+            new_level = self.levels[guild_id]["members"][member_id]["level"]
+            await message.channel.send(f"{message.author.mention}, you leveled up to **{new_level}**!")
 
-        self.saveLevels()
+        self.save_levels()
 
     @app_commands.command(name="level", description="Shows the level and XP of a user.")
     @app_commands.describe(member="The user whose level you want to see.")
@@ -73,46 +73,49 @@ class Leveling(commands.Cog):
         if member is None:
             member = interaction.user
 
-        guildId = str(interaction.guild.id)
-        memberId = str(member.id)
+        guild_id = str(interaction.guild.id)
+        member_id = str(member.id)
 
-        if guildId in self.levels and memberId in self.levels[guildId]["members"]:
-            level = self.levels[guildId]["members"][memberId]["level"]
-            xp = self.levels[guildId]["members"][memberId]["xp"]
-            requiredXp = self.getRequiredXp(level)
-            remainingXp = requiredXp - xp
+        if guild_id in self.levels and member_id in self.levels[guild_id]["members"]:
+            level = self.levels[guild_id]["members"][member_id]["level"]
+            xp = self.levels[guild_id]["members"][member_id]["xp"]
+            required_xp = self.get_required_xp(level)
+            remaining_xp = required_xp - xp
             await interaction.response.send_message(
-                f"{member.mention} is at level **{level}** with **{xp}** XP.\nNeeds **{remainingXp} XP** more to level up!"
+                f"{member.mention} is at level **{level}** with **{xp}** XP.\nNeeds **{remaining_xp} XP** more to level up!"
             )
         else:
             await interaction.response.send_message(f"{member.mention} has no levels recorded yet.")
 
     @app_commands.command(name="leaderboard", description="Shows the ranking of users with the most levels.")
     async def leaderboard(self, interaction: discord.Interaction):
-        guildId = str(interaction.guild.id)
+        guild_id = str(interaction.guild.id)
 
-        if guildId not in self.levels or "members" not in self.levels[guildId]:
+        if guild_id not in self.levels or "members" not in self.levels[guild_id]:
             await interaction.response.send_message("There is no level data for this server yet.")
             return
 
-        sortedLevels = sorted(
-            self.levels[guildId]["members"].items(), key=lambda x: x[1]['level'], reverse=True  # Ordena por nível
+        sorted_levels = sorted(
+            self.levels[guild_id]["members"].items(),
+            key=lambda x: (x[1]['level'], x[1]['xp']),
+            reverse=True
         )
 
         leaderboard = []
-        for i, (memberId, data) in enumerate(sortedLevels[:10], start=1):
-            user = self.bot.get_user(int(memberId))
+        for i, (member_id, data) in enumerate(sorted_levels[:10], start=1):
+            user = await self.bot.fetch_user(int(member_id))
             if user:
                 if i == 1:
                     emoji = "🥇"
+                    leaderboard.append(f"{emoji} **{user.name}** - Level: {data['level']} | XP: {data['xp']}")
                 elif i == 2:
                     emoji = "🥈"
+                    leaderboard.append(f"{emoji} **{user.name}** - Level: {data['level']} | XP: {data['xp']}")
                 elif i == 3:
                     emoji = "🥉"
+                    leaderboard.append(f"{emoji} **{user.name}** - Level: {data['level']} | XP: {data['xp']}")
                 else:
-                    emoji = "🏅"
-
-                leaderboard.append(f"{emoji} **{i}. {user.name}** - Level: {data['level']} | XP: {data['xp']}")
+                    leaderboard.append(f"▫️ **{i}. {user.name}** - Level: {data['level']} | XP: {data['xp']}")
 
         if leaderboard:
             embed = discord.Embed(
@@ -128,15 +131,15 @@ class Leveling(commands.Cog):
     @level.error
     @leaderboard.error
     async def error(self, interaction: discord.Interaction, error: app_commands.AppCommandError):
-        errorMessage = f"An error occurred: {error}"
+        error_message = f"An error occurred: {error}"
 
         print(f"{Fore.GREEN}[ERROR]{Style.RESET_ALL} {error}")
 
         try:
             if not interaction.response.is_done():
-                await interaction.response.send_message(errorMessage, ephemeral=True)
+                await interaction.response.send_message(error_message, ephemeral=True)
             else:
-                await interaction.followup.send_message(errorMessage, ephemeral=True)
+                await interaction.followup.send_message(error_message, ephemeral=True)
         except Exception as e:
             print(f"[ERROR] Failed to send error message: {e}")
 
